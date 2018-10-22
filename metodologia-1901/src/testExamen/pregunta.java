d0ddddb5f14863b8a8f32c2ed1f5e0fbef6ff948
T-107 <T-107@PC205>
2018-10-22 17:45:30 -0500

    Examen RiveroCastilloVictorManuel

--- metodologia-1901/src/testExamen/pregunta.java
+++ metodologia-1901/src/testExamen/pregunta.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//Examen Rivero Castillo Victor Manuel
 package testExamen;
 
 import javax.swing.JOptionPane;
